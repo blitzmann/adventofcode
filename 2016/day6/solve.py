bed2c62eb96d6530e041fdf56b576849b847d180
Ryan Holmes <holmes.ryan.90@gmail.com>
2017-06-04 02:28:51 -0400

    Create solve.py

---
+++ 2016/day6/solve.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+lines = []
+
+with open('input.txt', 'r') as file:
+    for line in file:
+        lines.append(line)
+
+# Part 1
+
+answer = ''
+for i in xrange(len(lines[0])-1):
+    answer += Counter([x[i] for x in lines]).most_common(1)[0][0]
+print answer
+
+# Part 2
+
+answer = ''
+for i in xrange(len(lines[0])-1):
+    answer += Counter([x[i] for x in lines]).most_common()[-1][0]
+print answer
+
