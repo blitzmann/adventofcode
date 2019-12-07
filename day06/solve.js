63d8c12454121f3160889f8395e77b56778d01be
blitzmann <holmes.ryan.90@gmail.com>
2019-12-06 23:14:51 -0500

    Day 6 part 2 and documentation

--- day06/solve.js
+++ day06/solve.js
@@ -2,52 +2,69 @@ const fs = require('fs');
 const path = require("path");
 const arr = fs.readFileSync(path.resolve(__dirname, 'input.txt')).toString().split('\n').map(i => i.trim().split(')'));
 
-const origin = new Map()
-const reverse = new Map()
-let map = origin;
+// todo: re-write this entire thing in an actual graph library
 
-for (let c of arr){
-    let thing = map.get(c[0])
-    if (!thing){
-        thing = []
-        map.set(c[0], thing)
+// stores orbited object as key, and it's orbiting objects as an array of values
+// This allows us to traverse the tree a little easier
+// Format: {parent: [child, ...]
+const map = new Map() 
+// this keeps track of the liniear path back to COM (since each node only has 1 parent)
+// Format: {child: parent}
+const reverse = new Map()
+
+for (let [from, to] of arr){
+    let children = map.get(from)
+    if (!children){
+        children = []
+        map.set(from, children)
     }
     
-    thing.push([c[1], c[0]])
-
-    
-        reverse.set(c[1], c[0])
-
-
+    children.push(to)
+    reverse.set(to, from)
 }
 
 let i = 0;
-function count(parent, num){
+function countOrbits(parent, num) {
+    // num keeps track of how many orbits to get to this parent
+    // i keeps track of total umber of orbits visited
     const children = map.get(parent);
-    num += 1
-    i += num;
-
-    if (!children) {  return; }
+    num++; // increment by one, since we've gone one step further in the tree
+    i += num; // increate total number of orbits by num (because it's cumulative, not incremental, per node)
     
-    for(let [child, _] of children) {
-        count(child, num);
+    if (!children) { return; } // nothing to do
+    
+    for(let child of children) { // for each child of this node, recurse
+        countOrbits(child, num);
     }
 }
 
-count("COM", -1)
+// start at COM, -1 count
+countOrbits("COM", -1)
 
 console.log(`Part 1: ${i}`);
 
-// function generateSet(node){
-//     const set = new Set();
+// Part two lends itself well to generating a Set representing the return path from a node back to COM
+// we can thern intersect tweo of these sets to find the merge location, and then count how many 
+// transfers from node to merge point
+function returnToCom(node){
+    const set = new Set();
 
-//     function recurse(node) {
-//         set.add(node)
-//         recurse(reverse.get(node))
-//     }
-//     recurse(reverse.get(node))
-// }
+    function recurse(node) {
+        if (!node) {return}
+        set.add(node)
+        recurse(reverse.get(node))
+    }
+    recurse(reverse.get(node))
+    return set
+}
 
-// console.log(generateSet("SAN"))
+const s = returnToCom("SAN");
+const y = returnToCom("YOU");
+const intersection = new Set([...s].filter(x => y.has(x)));
+const merge = Array.from(intersection)[0]
+numS = Array.from(s).indexOf(merge)
+numY = Array.from(y).indexOf(merge)
+
+console.log(`Part 2: ${numS + numY}`)
 
 
