6bd6b102e3f8da0317f01d0dd3cd1ec5ee93e187
blitzmann <holmes.ryan.90@gmail.com>
2022-12-03 00:37:00 -0500

    day 3 cleanup

--- 2022/day3/day3.js
+++ 2022/day3/day3.js
@@ -3,26 +3,7 @@ const text = fs.readFileSync("input.txt").toString();
 
 const lines = text.split("\r\n");
 
-result = lines.map((line) => {
-    let index = line.length / 2;
-    let [one, two] = [
-        new Set(line.slice(0, index).split("")),
-        new Set(line.slice(index).split("")),
-    ];
-
-    intersection = [...new Set([...one].filter((x) => two.has(x)))];
-    charCode = intersection[0].charCodeAt(0);
-    if (charCode >= "a".charCodeAt(0)) {
-        charCode -= 96;
-    } else {
-        charCode = charCode - 96 + 26 + 26 + 6;
-    }
-    return charCode;
-});
-
-console.log(result);
-console.log(result.reduce((a, b) => a + b, 0));
-
+// stack overflow :D
 function groupArr(data, n) {
     var group = [];
     for (var i = 0, j = 0; i < data.length; i++) {
@@ -33,23 +14,41 @@ function groupArr(data, n) {
     return group;
 }
 
+function score(char) {
+    // can't just use the char code for this, since the scoring take priority of lowercase
+    charCode = char.charCodeAt(0);
+    return (charCode -= charCode >= "a".charCodeAt(0) ? 96 : 38);
+}
+
+part1 = lines
+    .map((line) => {
+        let index = line.length / 2;
+        let [one, two] = [
+            new Set(line.slice(0, index).split("")),
+            new Set(line.slice(index).split("")),
+        ];
+
+        intersection = [...new Set([...one].filter((x) => two.has(x)))];
+        return score(intersection[0]);
+    })
+    .reduce((a, b) => a + b, 0);
+
+console.log("Part 1: ", part1);
+
 groups = groupArr(lines, 3);
+part2 = groups
+    .map((group) => {
+        let intersection = new Set(group[0].split(""));
+        for (let line of group.slice(1)) {
+            intersection = new Set(
+                [...intersection].filter((x) => new Set(line.split("")).has(x))
+            );
+        }
 
-part2 = groups.map((group) => {
-    set1 = new Set(group[0]);
-    set2 = new Set(group[1]);
-    set3 = new Set(group[2]);
+        intersection = [...intersection];
 
-    intersection = new Set([...set1].filter((x) => set2.has(x)));
-    intersection = [...new Set([...intersection].filter((x) => set3.has(x)))];
+        return score(intersection[0]);
+    })
+    .reduce((a, b) => a + b, 0);
 
-    charCode = intersection[0].charCodeAt(0);
-    if (charCode >= "a".charCodeAt(0)) {
-        charCode -= 96;
-    } else {
-        charCode = charCode - 96 + 26 + 26 + 6;
-    }
-    return charCode;
-});
-
-console.log(part2.reduce((a, b) => a + b, 0));
+console.log("Part 2: ", part2);
