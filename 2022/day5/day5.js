0e84841e7f592df5a3466e82240030ae2db071cc
blitzmann <holmes.ryan.90@gmail.com>
2022-12-05 00:37:29 -0500

    Clean up day 5

--- 2022/day5/day5.js
+++ 2022/day5/day5.js
@@ -4,53 +4,61 @@ const text = fs.readFileSync("input.txt").toString();
 
 const [top, bottom] = text.split("\r\n\r\n").map((x) => x.split("\r\n"));
 
-processed = top.map((line) => line.match(/.{1,4}/g).map((x) => x.trim()));
+top.pop(); // delete the junk stack labeling
 
-processed.reverse();
-processed.shift(); // delete the junk stack labeling
+processed = top
+    .map((line) => line.match(/.{1,4}/g).map((x) => x.trim()))
+    .reverse();
 
-const state = new Map();
+function processData(processed) {
+    let state = new Map();
 
-for (let i = 0; i < processed.length; i++) {
-    for (let j = 0; j < processed[i].length; j++) {
-        thing = processed[i][j];
-        let bucket = state.get(j);
-        if (!state.has(j)) {
-            bucket = [];
-            state.set(j, bucket);
-        }
-        letter = thing.match(/\w/g);
-        if (letter) {
-            bucket.push(letter[0]);
+    // process data into a Map<stackNum, crates[]>
+    for (let i = 0; i < processed.length; i++) {
+        for (let j = 1; j <= processed[i].length; j++) {
+            let bucket = state.get(j);
+            if (!bucket) {
+                bucket = [];
+                state.set(j, bucket);
+            }
+            letter = processed[i][j - 1].match(/\w/g);
+            if (letter) {
+                bucket.push(letter[0]);
+            }
         }
     }
+    return state;
 }
 
-// for (let ins of bottom) {
-//     const [_, num, from, to] = ins.match(/move (\d+) from (\d+) to (\d+)/);
-//     fromBucket = state.get(+from - 1);
-//     toBucket = state.get(+to - 1);
-//     for (let i = 0; i < num; i++) {
-//         toBucket.push(fromBucket.pop());
-//     }
-// }
-
-// let part1 = "";
-// for (let val of state.keys()) {
-//     part1 += state.get(val).pop();
-// }
-// console.log(part1);
+let state = processData(processed);
 
 for (let ins of bottom) {
     const [_, num, from, to] = ins.match(/move (\d+) from (\d+) to (\d+)/);
-    fromBucket = state.get(+from - 1);
-    toBucket = state.get(+to - 1);
-    let chunck = fromBucket.splice(-+num, num);
-    toBucket.push(...chunck);
+    fromBucket = state.get(+from);
+    toBucket = state.get(+to);
+    for (let i = 0; i < num; i++) {
+        toBucket.push(fromBucket.pop());
+    }
 }
 
 let part1 = "";
 for (let val of state.keys()) {
     part1 += state.get(val).pop();
 }
-console.log(part1);
+console.log("Part 1: ", part1);
+
+state = processData(processed);
+
+for (let ins of bottom) {
+    const [_, num, from, to] = ins.match(/move (\d+) from (\d+) to (\d+)/);
+    fromBucket = state.get(+from);
+    toBucket = state.get(+to);
+    let chunk = fromBucket.splice(-+num, num);
+    toBucket.push(...chunk);
+}
+
+let part2 = "";
+for (let val of state.keys()) {
+    part2 += state.get(val).pop();
+}
+console.log("Part 2: ", part2);
