930a4ef7801651ea986f9395a0f76e129a975683
blitzmann <holmes.ryan.90@gmail.com>
2022-12-10 21:56:17 -0500

    day 9, part 2

--- 2022/day9/day9.js
+++ 2022/day9/day9.js
@@ -3,89 +3,137 @@ const text = fs.readFileSync("input.txt").toString();
 
 const lines = text.split("\r\n");
 
-let head = [0, 0];
-let tail = [0, 0];
-const tailPoints = new Set();
-
 const calculateDistance = (a, b) => {
     return Math.max(Math.abs(a[0] - b[0]), Math.abs(a[1] - b[1]));
 };
 
-function printState() {
-    // find min / max of grid
-    let minX = 0;
-    let maxX = 6;
-    let minY = 0;
-    let maxY = 6;
+class Rope {
+    constructor(size) {
+        this.segments = Array(size)
+            .fill()
+            .map((_) => [0, 0]);
+        this.size = size;
+        this.tailPoints = new Set();
+    }
 
-    for (let y = maxY; y >= minY; y--) {
-        let line = "";
-        for (let x = minX; x <= maxX; x++) {
-            if (head[0] === x && head[1] === y) {
-                line += "H";
-            } else if (tail[0] === x && tail[1] === y) {
-                line += "T";
-            } else if (y === 0 && x === 0) {
-                line += "s";
-            } else {
-                line += ".";
+    calc(dir, num) {
+        for (let i = 0; i < +num; i++) {
+            // console.log(`== ${dir} ${i + 1}/${num} ==`);
+            for (let j = 0; j < this.size - 1; j++) {
+                let head = this.segments[j];
+                let tail = this.segments[j + 1]; // "tail" in the context of these two segments
+
+                if (j === 0) {
+                    if (dir === "U") {
+                        this.segments[j][1] += 1;
+                    }
+                    if (dir === "D") {
+                        this.segments[j][1] -= 1;
+                    }
+                    if (dir === "R") {
+                        this.segments[j][0] += 1;
+                    }
+                    if (dir === "L") {
+                        this.segments[j][0] -= 1;
+                    }
+                }
+                if (
+                    calculateDistance(this.segments[j], this.segments[j + 1]) >
+                    1
+                ) {
+                    this.doPhysics(head, tail);
+                }
             }
+
+            this.tailPoints.add(`X${this.tail[0]}Y${this.tail[1]}`);
+            // this.printState();
         }
-        console.log(line);
+    }
+
+    get head() {
+        return this.segments[0];
+    }
+
+    get tail() {
+        return this.segments[this.size - 1];
+    }
+
+    doPhysics(head, tail) {
+        // determine diagonal steps
+        if (head[0] !== tail[0] && head[1] !== tail[1]) {
+            if (head[0] > tail[0] && head[1] > tail[1]) {
+                tail[0] += 1;
+                tail[1] += 1;
+            }
+            if (head[0] < tail[0] && head[1] > tail[1]) {
+                tail[0] -= 1;
+                tail[1] += 1;
+            }
+            if (head[0] > tail[0] && head[1] < tail[1]) {
+                tail[0] += 1;
+                tail[1] -= 1;
+            }
+            if (head[0] < tail[0] && head[1] < tail[1]) {
+                tail[0] -= 1;
+                tail[1] -= 1;
+            }
+        } else if (head[1] > tail[1]) {
+            // if head is above tail
+            tail[1] = head[1] - 1;
+        } else if (head[1] < tail[1]) {
+            // if head is below tail
+            tail[1] = head[1] + 1;
+        } else if (head[0] < tail[0]) {
+            // if head is left of tail
+            tail[0] = head[0] + 1;
+        } else if (head[0] > tail[0]) {
+            // if head is right of tail
+            tail[0] = head[0] - 1;
+        }
+    }
+
+    printState() {
+        // find min / max of grid
+        let minX = -15;
+        let maxX = 15;
+        let minY = -18;
+        let maxY = 18;
+
+        for (let y = maxY; y >= minY; y--) {
+            let line = "";
+            for (let x = minX; x <= maxX; x++) {
+                let v = this.segments.indexOf(
+                    this.segments.find((z) => z[0] === x && z[1] === y)
+                );
+                if (v >= 0) {
+                    if (v === 0) {
+                        line += "H";
+                    } else {
+                        line += v.toString();
+                    }
+                } else if (y === 0 && x === 0) {
+                    line += "s";
+                } else {
+                    line += ".";
+                }
+            }
+            console.log(line + " " + y);
+        }
     }
 }
 
-function doPhysics(head, tail) {
-    if (Math.abs(head[1] - tail[1]) > 1) {
-        // the head has moved up or down, the tail is forced to be in the same column
-        tail[0] = head[0];
-    } else if (Math.abs(head[0] - tail[0]) > 1) {
-        // the head has moved left or right, the tail is forced to be in the same row
-        tail[1] = head[1];
-    }
-
-    if (head[1] > tail[1]) {
-        // if head is above tail
-        tail[1] = head[1] - 1;
-    } else if (head[1] < tail[1]) {
-        // if head is below tail
-        tail[1] = head[1] + 1;
-    } else if (head[0] < tail[0]) {
-        // if head is left of tail
-        tail[0] = head[0] + 1;
-    } else if (head[0] > tail[0]) {
-        // if head is right of tail
-        tail[0] = head[0] - 1;
-    }
-}
-
+let rope = new Rope(2);
 for (let line of lines) {
     let [dir, num] = line.split(" ");
-    for (let i = 0; i < +num; i++) {
-        console.log(`== ${dir} ${i + 1}/${num} ==`);
-        if (dir === "U") {
-            head = [head[0], head[1] + 1];
-        }
-        if (dir === "D") {
-            head = [head[0], head[1] - 1];
-        }
-        if (dir === "R") {
-            head = [head[0] + 1, head[1]];
-        }
-        if (dir === "L") {
-            head = [head[0] - 1, head[1]];
-        }
-
-        if (calculateDistance(head, tail) > 1) {
-            doPhysics(head, tail);
-        }
-        // console.log("==== " + dir + i + " ====");
-        // console.log("head: ", head);
-        // console.log("tail: ", tail);
-
-        tailPoints.add(`X${tail[0]}Y${tail[1]}`);
-        printState();
-    }
+    rope.calc(dir, +num);
 }
 
-console.log(tailPoints.size);
+console.log("part 1: ", rope.tailPoints.size);
+
+rope = new Rope(10);
+for (let line of lines) {
+    let [dir, num] = line.split(" ");
+    rope.calc(dir, +num);
+}
+
+console.log("part 2: ", rope.tailPoints.size);
