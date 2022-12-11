e0c1d85b3ef2275b369c5a715e8e9349c79068eb
blitzmann <holmes.ryan.90@gmail.com>
2022-12-10 20:46:29 -0500

    reverted back to day 9 part 1 solve code

--- 2022/day9/day9.js
+++ 2022/day9/day9.js
@@ -3,96 +3,82 @@ const text = fs.readFileSync("input.txt").toString();
 
 const lines = text.split("\r\n");
 
-// let head = [0, 0];
-// let tail = [0, 0];
-// const tailPoints = new Set();
+let head = [0, 0];
+let tail = [0, 0];
+const tailPoints = new Set();
 
 const calculateDistance = (a, b) => {
     return Math.max(Math.abs(a[0] - b[0]), Math.abs(a[1] - b[1]));
 };
 
-// for (let line of lines) {
-//     let [dir, num] = line.split(" ");
+function printState() {
+    // find min / max of grid
+    let minX = 0;
+    let maxX = 6;
+    let minY = 0;
+    let maxY = 6;
 
-//     }
-// }
-
-// console.log(tailPoints.size);
-
-// part 2
-
-class Rope {
-    constructor(size) {
-        this.segments = Array(size)
-            .fill()
-            .map((_) => [0, 0]);
-        this.size = size;
-        this.tailPoints = new Set();
-    }
-
-    calc(dir, num) {
-        for (let i = 0; i < +num; i++) {
-            for (let j = 0; j < this.size - 1; j++) {
-                let head = this.segments[j];
-                let tail = this.segments[j + 1]; // "tail" in the context of these two segments
-                if (j === 0) {
-                    if (dir === "U") {
-                        this.segments[j] = [head[0], head[1] + 1];
-                    }
-                    if (dir === "D") {
-                        this.segments[j] = [head[0], head[1] - 1];
-                    }
-                    if (dir === "R") {
-                        this.segments[j] = [head[0] + 1, head[1]];
-                    }
-                    if (dir === "L") {
-                        this.segments[j] = [head[0] - 1, head[1]];
-                    }
-                }
-                if (
-                    calculateDistance(this.segments[j], this.segments[j + 1]) >
-                    1
-                ) {
-                    if (dir === "U" || dir === "D") {
-                        tail[0] = head[0];
-                    } else {
-                        tail[1] = head[1];
-                    }
-
-                    if (dir === "U") {
-                        tail[1] = head[1] - 1;
-                    }
-                    if (dir === "D") {
-                        tail[1] = head[1] + 1;
-                    }
-                    if (dir === "R") {
-                        tail[0] = head[0] - 1;
-                    }
-                    if (dir === "L") {
-                        tail[0] = head[0] + 1;
-                    }
-                }
+    for (let y = maxY; y >= minY; y--) {
+        let line = "";
+        for (let x = minX; x <= maxX; x++) {
+            if (head[0] === x && head[1] === y) {
+                line += "H";
+            } else if (tail[0] === x && tail[1] === y) {
+                line += "T";
+            } else if (y === 0 && x === 0) {
+                line += "s";
+            } else {
+                line += ".";
             }
-            // console.log("==== " + dir + i + " ====");
-            // console.log("head: ", head);
-            // console.log("tail: ", tail);
-
-            this.tailPoints.add(`X${this.tail[0]}Y${this.tail[1]}`);
         }
-    }
-
-    get head() {
-        return this.segments[0];
-    }
-    get tail() {
-        return this.segments[this.size - 1];
+        console.log(line);
     }
 }
 
-const rope = new Rope(9);
 for (let line of lines) {
     let [dir, num] = line.split(" ");
-    rope.calc(dir, +num);
+    for (let i = 0; i < +num; i++) {
+        console.log(`== ${dir} ${i + 1}/${num} ==`);
+        if (dir === "U") {
+            head = [head[0], head[1] + 1];
+        }
+        if (dir === "D") {
+            head = [head[0], head[1] - 1];
+        }
+        if (dir === "R") {
+            head = [head[0] + 1, head[1]];
+        }
+        if (dir === "L") {
+            head = [head[0] - 1, head[1]];
+        }
+
+        if (calculateDistance(head, tail) > 1) {
+            if (dir === "U" || dir === "D") {
+                tail[0] = head[0];
+            } else {
+                tail[1] = head[1];
+            }
+
+            if (dir === "U") {
+                tail[1] = head[1] - 1;
+            }
+            if (dir === "D") {
+                tail[1] = head[1] + 1;
+            }
+            if (dir === "R") {
+                tail[0] = head[0] - 1;
+            }
+            if (dir === "L") {
+                tail[0] = head[0] + 1;
+            }
+        }
+        // console.log("==== " + dir + i + " ====");
+        // console.log("head: ", head);
+        // console.log("tail: ", tail);
+
+        tailPoints.add(`X${tail[0]}Y${tail[1]}`);
+        printState();
+    }
 }
 
-console.log(rope.tailPoints, rope.tailPoints.size);
+console.log(tailPoints.size);
