c825abe1229a53722933172b9d7bb0fb5be0ca50
blitzmann <holmes.ryan.90@gmail.com>
2023-12-01 09:29:11 -0500

    tweaks to 2023.1

--- 2023/day1/day1.js
+++ 2023/day1/day1.js
@@ -25,29 +25,31 @@ let numbers = [
 
 console.log(
     lines
-        .map(replacePhonetic)
+        .map(replaceWords)
         .map((x) => x.replace(/[^\d]+/g, ""))
         .map((x) => x.substring(0, 1) + x.slice(-1))
         .map(Number)
         .reduce((a, b) => a + b, 0)
 );
 
-function replacePhonetic(string) {
-    // we need to find the index of each and replace the latest one
+function replaceWords(string) {
+    // we need to find the index of each and replace the first one found. We do this by tracking the index.
+    // we use a while loop as we have to get all words. Each iteration will replace the first instance of the word.
     while (true) {
         let idx = Infinity;
         let selected = null;
-        for (let ph of numbers) {
-            let testIdx = string.indexOf(ph);
+        for (let word of numbers) {
+            let testIdx = string.indexOf(word);
             if (testIdx > -1 && testIdx < idx) {
                 idx = testIdx;
-                selected = ph;
+                selected = word;
             }
         }
         if (idx === Infinity) {
+            // no more words found, break out of while loop
             break;
         }
-
+        // we know which word we need to replace now, replace it and start over.
         string = string.replace(selected, numbers.indexOf(selected) + 1);
     }
     return string;
