5d326a1db830ad45ea8a51ac8bc6906bf866b2fa
blitzmann <holmes.ryan.90@gmail.com>
2023-12-04 09:26:16 -0500

    combine logic of part 1 and 2, streamline some things, and document

--- 2023/day4/day4.js
+++ 2023/day4/day4.js
@@ -2,63 +2,43 @@ const fs = require("fs");
 const text = fs.readFileSync("input.txt").toString();
 
 const lines = text.split("\r\n");
-const matrix = lines.map((x) => x.split(""));
-const isPartMatrix = lines.map((x) => x.split("").map((x) => null));
 
 let totalSum = 0;
+const copies = new Array(lines.length).fill(1);
 
-for (let line of lines) {
-    let [card, numbers] = line.split(":");
+for (let [x, line] of lines.entries()) {
+    let [_, numbers] = line.split(":");
     let [winning, have] = numbers
         .split("|")
         .map((x) => x.trim().split(/\s+/).map(Number));
 
-    let numofWinning = have.map((have) =>
-        winning.filter((x) => x === have).length > 0 ? 1 : 0
-    );
-    numofWinning = numofWinning.reduce((a, b) => a + b, 0);
-    if (numofWinning !== 0) {
-        totalSum += Math.pow(2, numofWinning - 1);
-    }
-    // let intersect = [...have].filter((i) => new Set(winning).has(i));
-    // let intersectSet = new Set(
-    //     [...have].filter((i) => new Set(winning).has(i))
-    // );
+    // determine how many winning numbers we have.
+    let numofWinning = have.filter((have) => winning.includes(have)).length;
 
-    // if (intersect.length !== intersectSet.size) {
-    //     console.log("sdasdf");
-    // }
-    // if (intersect.size !== 0) {
-    //     totalSum += Math.pow(2, intersect.length - 1);
-    // }
-}
-
-console.log(totalSum);
-
-const copies = new Array(lines.length).fill(0);
-
-for (let x = 0; x < lines.length; x++) {
-    let line = lines[x];
-    let [card, numbers] = line.split(":");
-    let [winning, have] = numbers
-        .split("|")
-        .map((x) => x.trim().split(/\s+/).map(Number));
-
-    let numofWinning = have.map((have) =>
-        winning.filter((x) => x === have).length > 0 ? 1 : 0
-    );
-    numofWinning = numofWinning.reduce((a, b) => a + b, 0);
+    // determine how many times we need to run our win logic (part 2)
     let myCopies = copies[x];
-    // +1 to handle the current, original.
-    for (let j = 0; j < myCopies + 1; j++) {
-        if (numofWinning > 0) {
-            for (let i = 0; i < numofWinning; i++) {
-                if (x + 1 + i <= copies.length - 1) {
-                    copies[x + 1 + i]++;
+
+    if (numofWinning !== 0) {
+        // part 1
+        // keep running sum of points
+        totalSum += Math.pow(2, numofWinning - 1);
+
+        // part 2
+        // for each copy of this card, we must run the logic to allocate new copies.
+        for (let j = 0; j < myCopies; j++) {
+            // for each winning card,
+            for (let i = 1; i <= numofWinning; i++) {
+                let targetCard = x + i;
+                // only target cards that are in our array
+                // (rules state "Cards will never make you copy a card past the end of the table" but I don't know if this is baked into the input
+                // or if the logic should just be ignored for ones that are out of range)
+                if (targetCard <= copies.length - 1) {
+                    copies[targetCard]++;
                 }
             }
         }
     }
 }
 
-console.log(lines.length + copies.reduce((a, b) => a + b, 0));
+console.log(totalSum);
+console.log(copies.reduce((a, b) => a + b, 0));
